b6b3b4a17bb92fc4216fd306e891f95372a4ebce
Moritz Hoffmann <moritz1@gmx.com>
2018-09-07 18:30:47 +0200

    Add dataset deletion

--- src/routes/list.js
+++ src/routes/list.js
@@ -6,6 +6,7 @@ import {
 	Button,
 	Alert,
 	Table, TableRow,
+	Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
 
 import { PersonIcon, ShieldIcon } from 'react-octicons';
@@ -20,10 +21,14 @@ import BootstrapTable from 'react-bootstrap-table-next';
 
 import { PacmanLoader as Loader } from 'react-spinners';
 
+import { TrashcanIcon } from 'react-octicons';
+
 class ListPage extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
+			modalID: null,
+			modal: false,
 			ready: false,
 			rows: [],
 			columns: [
@@ -45,7 +50,11 @@ class ListPage extends Component {
 					dataField: 'id',
 					text: 'Document ID',
 					formatter: (cell, row) => (
-						<a href={`/dataset/${row.id}`}>{cell}</a>
+						<div>
+							<a href="#" onClick={() => this.deleteHandler(row.id)}><TrashcanIcon /></a>
+							{'  '}
+							<a href="#" onClick={() => this.props.history.push(`dataset/${row.id}`)}>{cell}</a>
+						</div>
 					),
 				}
 			],
@@ -55,6 +64,46 @@ class ListPage extends Component {
 			}],
 			loading: true,
 		};
+		this.deleteHandler = this.deleteHandler.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
+		this.modalDeleteHandler = this.modalDeleteHandler.bind(this);
+	}
+
+	deleteHandler(id){
+		this.setState(update(this.state, {
+			modalID: {$set: id},
+			modal: {$set: !this.state.modal},
+		}));
+	}
+
+	modalDeleteHandler(){
+		if(this.state.modalID === null){
+			this.toggleModal();
+			return;
+		}
+
+		const options = {
+			method: 'DELETE',
+			url: `https://edge.aura.rest/dataset/${this.state.modalID}`,
+			headers: {
+				Authorization: `Bearer ${window.localStorage.getItem('id_token')}`
+			},
+		};
+
+		Request(options).then((res) => {
+			const index = this.state.rows.findIndex(row => row.id === this.state.modalID);
+			this.setState(update(this.state, {
+				modalID: {$set: null},
+				modal: {$set: false},
+				rows: {$splice: [[index, 1]]},
+			}));
+		});
+	}
+
+	toggleModal(){
+		this.setState(update(this.state, {
+			modal: {$set: !this.state.modal},
+		}));
 	}
 
 	componentDidMount(){
@@ -74,10 +123,8 @@ class ListPage extends Component {
 				i++;
 			});
 			this.setState(update(this.state, {
-				$merge: {
-					ready: true,
-					rows: res,
-				}
+				ready: {$set: true},
+				rows: {$set: res},
 			}));
 		});
 	}
@@ -98,6 +145,16 @@ class ListPage extends Component {
 								columns={ this.state.columns }
 								hover={true}
 							/>
+							<Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+								<ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
+								<ModalBody>
+									O RLY delete {this.state.modalID}?
+								</ModalBody>
+								<ModalFooter>
+									<Button color="danger" onClick={this.modalDeleteHandler}>Yes</Button>{' '}
+									<Button color="secondary" onClick={this.toggleModal}>No</Button>
+								</ModalFooter>
+							</Modal>
 						</Col>
 					):(
 						<Col sm={{ size: 2, offset: 4 }}>
