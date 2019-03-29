f58f5709fefd550527021acb1c0b4a97975a894b
Moritz Hoffmann <moritz1@gmx.com>
2019-03-29 21:17:54 +0100

    changes

--- backend-src/server.js
+++ backend-src/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const http = require('http');
+const fs   = require('fs');
 
 const Koa = require('koa');
 const socketio = require('socket.io');
@@ -12,8 +13,10 @@ const server = http.createServer(app.callback());
 
 const io = socketio(server);
 
+const labelingsPath = path.join(__dirname, '../', 'config', 'labelings.json');
+
 const auth = require(path.join(__dirname, '../', 'config', 'auth.json'));
-let labelings = require(path.join(__dirname, '../', 'config', 'labelings.json'));
+let labelings = require(labelingsPath);
 
 SocketIoAuth(io, {
 	authenticate: (socket, data, callback) => callback(null, (auth[data.username] === data.password)),
@@ -28,13 +31,20 @@ io.on('connection', (socket) => {
 	console.log('client connected');
 
 	socket.on('labelings', (newLabelings) => {
-		if (!newLabelings) {
-			console.log("sending")
+		if(!newLabelings){
+			console.log('labeling request');
 			socket.emit('labelings', labelings);
-		} else {
-			console.log("updating")
+		}else{
+			console.log('got new labelings');
 			labelings = newLabelings;
-			socket.emit('labelings', newLabelings);	
+
+			fs.writeFile(labelingsPath, JSON.stringify(labelings, null, '\t'), (err) => {
+				if(err){
+					console.error(err);
+				}
+			});
+
+			io.emit('labelings', newLabelings);
 		}
 	});
 
