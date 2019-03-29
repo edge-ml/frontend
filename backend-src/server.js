1e9018167618d0329fef1de22836b0004f600e36
Moritz Hoffmann <moritz1@gmx.com>
2019-03-29 16:03:44 +0100

    add labelings hook

--- backend-src/server.js
+++ backend-src/server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const http = require('http')
+const http = require('http');
 
 const Koa = require('koa');
 const socketio = require('socket.io');
@@ -13,22 +13,24 @@ const server = http.createServer(app.callback());
 const io = socketio(server);
 
 const auth = require(path.join(__dirname, '../', 'config', 'auth.json'));
+const labelings = require(path.join(__dirname, '../', 'config', 'labelings.json'));
 
 SocketIoAuth(io, {
 	authenticate: (socket, data, callback) => callback(null, (auth[data.username] === data.password)),
 	postAuthenticate: (socket, data) => {
 		console.log('authed');
 		// maybe store user info?
-		// socket.client.user = user;
+		socket.client.authed = true;
 	},
 });
 
 io.on('connection', (socket) => {
 	console.log('client connected');
 
-	/*socket.on('action', (msg) => {
-		process(msg);
-	});*/
+	socket.on('labelings', (msg) => {
+		console.log(`client: labelings >>> ${msg}`);
+		socket.emit('labelings', labelings);
+	});
 
 	socket.on('disconnect', () => {
 		console.log('client disconnected');
@@ -36,7 +38,7 @@ io.on('connection', (socket) => {
 });
 
 app.use(KoaStaticServer({
-	rootDir: '../build',
+	rootDir: path.join(__dirname, '../', 'build'),
 	index: 'index.html',
 }));
 
