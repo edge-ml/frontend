be98dd1ab9b2806fc6071d587b2836122160de41
33du <du.shanshan@outlook.com>
2020-01-03 17:51:14 +0100

    fixed updating label type bug

--- backend-src/EventComponents/LabelEventComponent.js
+++ backend-src/EventComponents/LabelEventComponent.js
@@ -21,7 +21,7 @@ function applyTo(io, socket) {
      */
     socket.on('labelings_labels', () => {
         if (!socket.client.twoFactorAuthenticated) return; // TODO: can we handle this more elegantly instead of calling it on every request which can be easily forgotten
-    
+
         Promise.all([
             rp(generateRequest(CONSTANTS.HTTP_METHODS.GET, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS)),
             rp(generateRequest(CONSTANTS.HTTP_METHODS.GET, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_TYPES))
@@ -29,13 +29,13 @@ function applyTo(io, socket) {
         .then(results => socket.emit('labelings_labels', {labelings: results[0], labels: results[1]}))
         .catch(err => console.log(err)); // TODO: handle error more meaningful, also why are we not emitting any errors here?
     });
-    
+
     /**
      * ADD Label Definition
      */
     socket.on('add_labeling', newLabeling => {
         if (!socket.client.twoFactorAuthenticated) return; //TODO: see above
-    
+
         rp(generateRequest(CONSTANTS.HTTP_METHODS.POST, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS, newLabeling))
         .then(() => rp(generateRequest(CONSTANTS.HTTP_METHODS.GET, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS))
             .then(labelings => {
@@ -45,13 +45,13 @@ function applyTo(io, socket) {
         )
         .catch(err => socket.emit('err', err));
     });
-    
+
     /**
      * UPDATE Label Definition
      */
     socket.on('update_labeling', labeling => {
         if (!socket.client.twoFactorAuthenticated) return;  //TODO: see above
-    
+
         rp(generateRequest(CONSTANTS.HTTP_METHODS.PUT, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS + `/${labeling['_id']}`, labeling))
         .then(() => rp(generateRequest(CONSTANTS.HTTP_METHODS.GET, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS)))
         .then(labelings => {
@@ -60,13 +60,12 @@ function applyTo(io, socket) {
         })
         .catch(err => socket.emit('err', err));
     });
-    
+
     /**
      * DELETE Label Definition
      */
     socket.on('delete_labeling', labelingId => {
         if (!socket.client.twoFactorAuthenticated) return;  //TODO: see above
-        console.log(labelingId)
         rp(generateRequest(CONSTANTS.HTTP_METHODS.DELETE, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_DEFINITIONS + `/${labelingId}`))
         .then(() => {
             Promise.all([
@@ -77,20 +76,20 @@ function applyTo(io, socket) {
         })
         .catch(err => socket.emit('err', err));
     });
-    
+
     /**
      * UPDATE Label Types of a Labeling
      */
     socket.on('update_labeling_labels', (labeling, labels, deletedLabels) => {
         if (!socket.client.twoFactorAuthenticated) return;  //TODO: see above
-    
+
         Promise.all(
             labels.filter(label => label.isNewLabel)
             .map(label => rp(generateRequest(CONSTANTS.HTTP_METHODS.POST, CONSTANTS.API_URI, CONSTANTS.ENDPOINTS.LABEL_TYPES, label))))
             .then(newLabels => {
             let newLabelsId = newLabels.map(label => label['_id']);
             labeling.labels = [ ...labeling.labels, ...newLabelsId ];
-    
+
             let promises = [];
             if (!labeling['_id']) {
                 promises = [
@@ -108,7 +107,7 @@ function applyTo(io, socket) {
                 ];
             } else {
                 let updatedLabels = labels.filter(label => label.updated);
-    
+
                 promises = [
                     ...promises,
                     rp({
@@ -123,7 +122,7 @@ function applyTo(io, socket) {
                     }),
                     ...updatedLabels.map(label => rp({
                         method: 'PUT',
-                        uri: CONSTANTS.API_URI + `/labelDefinitions/${label['_id']}`,
+                        uri: CONSTANTS.API_URI + `/labelTypes/${label['_id']}`,
                         headers: {
                             'User-Agent': 'Explorer',
                             'Authorization': CONSTANTS.ACCESS_TOKEN
@@ -133,7 +132,7 @@ function applyTo(io, socket) {
                     })),
                     ...deletedLabels.map(labelId => rp({
                         method: 'DELETE',
-                        uri: CONSTANTS.API_URI + `/labelDefinitions/${labelId}`,
+                        uri: CONSTANTS.API_URI + `/labelTypes/${labelId}`,
                         headers: {
                             'User-Agent': 'Explorer',
                             'Authorization': CONSTANTS.ACCESS_TOKEN
@@ -142,7 +141,7 @@ function applyTo(io, socket) {
                     }))
                 ];
             }
-    
+
             return Promise.all(promises);
         })
         .then(() =>{
