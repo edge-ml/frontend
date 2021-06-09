5a75839514f10e6cdbf3874894ee2535053a2def
Tobias King <tk.king.tk@gmail.com>
2021-06-09 18:40:54 +0200

    Added tests for autoCompleteInput

--- src/Tests/components/autoCompleteInput.test.js
+++ src/Tests/components/autoCompleteInput.test.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { mount, shallow } from "enzyme";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import AutoCompleteInput from "../../components/AutoCompleteInput/AutocompleteInput";
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import AutoCompleteInput from '../../components/AutoCompleteInput/AutocompleteInput';
 
-jest.mock("../../services/ApiServices/DatasetServices");
-jest.mock("../../services/CsvService");
+jest.mock('../../services/ApiServices/DatasetServices');
+jest.mock('../../services/CsvService');
 
 configure({ adapter: new Adapter() });
 
 function options() {
   return new Promise((resolve, reject) => {
-    resolve(["Option1", "Option2", "Oeption3"]);
+    resolve(['Option1', 'Option2', 'Oeption3']);
   });
 }
 
-it("render component", () => {
+it('render component', () => {
   const wrapper = mount(
     <AutoCompleteInput getSuggestions={options}></AutoCompleteInput>
   );
-  expect(wrapper.html()).not.toEqual("");
+  expect(wrapper.html()).not.toEqual('');
 });
 
-it("Putting in one letter and get suggestions", async () => {
+it('Putting in one letter and get suggestions', async () => {
   const onChange = jest.fn();
   const wrapper = mount(
     <AutoCompleteInput
@@ -31,16 +31,16 @@ it("Putting in one letter and get suggestions", async () => {
     ></AutoCompleteInput>
   );
   wrapper
-    .find("#autoCompleteInput")
+    .find('#autoCompleteInput')
     .first()
-    .simulate("change", { target: { value: "O" } });
+    .simulate('change', { target: { value: 'O' } });
   await flushPromises();
-  expect(wrapper.text().includes("Option1"));
-  expect(wrapper.text().includes("Option2"));
-  expect(wrapper.text().includes("Oeption3"));
+  expect(wrapper.text().includes('Option1'));
+  expect(wrapper.text().includes('Option2'));
+  expect(wrapper.text().includes('Oeption3'));
 });
 
-it("Click on one suggestion", async () => {
+it('Click on one suggestion', async () => {
   const onChange = jest.fn();
   const wrapper = mount(
     <AutoCompleteInput
@@ -49,24 +49,24 @@ it("Click on one suggestion", async () => {
     ></AutoCompleteInput>
   );
   wrapper
-    .find("#autoCompleteInput")
+    .find('#autoCompleteInput')
     .first()
-    .simulate("change", { target: { value: "O" } });
+    .simulate('change', { target: { value: 'O' } });
   await flushPromises();
   wrapper.update();
-  wrapper.find("#Option1").simulate("click");
+  wrapper.find('#Option1').simulate('click');
   await flushPromises();
   wrapper.update();
   expect(onChange).toHaveBeenCalled();
-  wrapper.setProps({ value: "Option1" });
+  wrapper.setProps({ value: 'Option1' });
   expect(
     wrapper
-      .find("#autoCompleteInput")
+      .find('#autoCompleteInput')
       .first()
       .props().value
-  ).toBe("Option1");
+  ).toBe('Option1');
 });
 
 function flushPromises() {
-  return new Promise((resolve) => setImmediate(resolve));
+  return new Promise(resolve => setImmediate(resolve));
 }
