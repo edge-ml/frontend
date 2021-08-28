25873186df98874980f46637021fa758770dec4f
Philipp <uukfn@student.kit.edu>
2021-08-28 19:51:45 +0200

    Implemented "Download as CSV".
    "Download as JSON" was changed to "Download as CSV" instead. Labels are not yet included in the CSV file though. (TODO).

--- src/components/ManagementPanel/ManagementPanel.js
+++ src/components/ManagementPanel/ManagementPanel.js
@@ -35,13 +35,51 @@ class ManagementPanel extends Component {
   }
 
   downloadDataSet() {
-    const fileName = this.props.dataset['_id'];
-    const json = JSON.stringify(this.props.dataset, undefined, 4);
-    const blob = new Blob([json], { type: 'application/json' });
+    const dataset = this.props.dataset;
+    const fileName = dataset.name;
+    var csv = 'time,';
+    var csv_lines = Object(); // this will be used as a dictionary, with timestamps as keys, and arrays of values as values
+    var timestamps = new Set([]); // this variable will hold all timestamps as an ordered array
+
+    // TODO: labelings are not yet added to the CSV file!
+
+    dataset.timeSeries.forEach(t => {
+      csv += 'sensor_' + t.name + '[' + t.unit + ']' + ',';
+    });
+    csv = csv.slice(0, -1); // remove the single ',' at the end
+    csv += '\r\n'; // this concludes the first csv line, now append data
+
+    // collect all timestamp values in a set, and initialize csv_lines to contain empty arrays as values
+    dataset.timeSeries.forEach(t => {
+      t.data.forEach(d => {
+        timestamps.add(d.timestamp);
+        csv_lines[d.timestamp] = [];
+      });
+    });
+    timestamps = Array.from(timestamps).sort();
+
+    dataset.timeSeries.forEach(t => {
+      var missingTimestamps = new Set(timestamps); // in the end, this array will contain all timestamps, that do not have a value -> ,, in CSV
+
+      t.data.forEach(d => {
+        csv_lines[d.timestamp].push(d.datapoint);
+        missingTimestamps.delete(d.timestamp); // since it's not missing, delete the timestamp from the missing timestamps
+      });
+
+      missingTimestamps.forEach(m => {
+        csv_lines[m].push(undefined); // all missing timestamps must result in empty values in the CSV (,,)
+      }); // pushing undefined and later doing .join(",") will results in this desired behavious
+    });
+
+    for (const [timestamp, values] of Object.entries(csv_lines)) {
+      csv += timestamp + ',' + values.join(',') + '\r\n';
+    }
+
+    const blob = new Blob([csv], { type: 'application/csv' });
     const href = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = href;
-    link.download = fileName + '.json';
+    link.download = fileName + '.csv';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -70,7 +108,7 @@ class ManagementPanel extends Component {
             color="primary"
             onClick={this.downloadDataSet}
           >
-            Download as JSON
+            Download as CSV
           </Button>
           <Button
             id="buttonDeleteDataset"
