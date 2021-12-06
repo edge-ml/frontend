b3f45187e92531798ce4a84d44ff6344e20eae60
KtrauM <murat.kurnaz12@gmail.com>
2021-12-06 13:16:09 +0300

    components for selection & boolean hyperparameters

---
+++ src/components/Hyperparameters/BooleanHyperparameter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Card,
+  CardBody,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Input
+} from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
+
+class BooleanHyperparameter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+export default BooleanHyperparameter;
