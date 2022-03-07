e8b950a8260826f6e1a62f7f5cf4927456ec448b
pir-anja <anja@ralfmartinhansen.de>
2022-03-08 00:18:15 +0100

    init sensor streaming

--- src/components/BLE/BlePanelRecordingDisplay.js
+++ src/components/BLE/BlePanelRecordingDisplay.js
@@ -1,53 +1,174 @@
-import React from 'react';
-import { Table } from 'reactstrap';
 import './BleActivated.css';
 
-function BlePanelRecordingDisplay(props) {
-  let indices = Array.from(props.selectedSensors);
+import React, { Component } from 'react';
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 
-  function separateComponents(dataArray) {
-    return Array.isArray(dataArray)
-      ? roundComponents(dataArray).join('; ')
-      : ' ';
+class BlePanelRecordingDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleStartLiveUpdate = this.handleStartLiveUpdate.bind(this);
+    this.handleStopLiveUpdate = this.handleStopLiveUpdate.bind(this);
+    this.updateLiveData = this.updateLiveData.bind(this);
+    this.generateStartSeries = this.generateStartSeries.bind(this);
+    this.generateStartData = this.generateStartData.bind(this);
+
+    this.interval_length = 200; // in ms
+    this.datastream_length = 20;
+    this.allOptions = [];
+
+    this.state = {
+      liveUpdate: false
+    };
+
+    //initialize HighCharts options for the different sensors
+    for (let i = 0; i < Object.keys(this.props.deviceSensors).length; i++) {
+      this.allOptions.push(
+        this.getOptions(
+          this.props.deviceSensors[i].parseScheme.map(elm => elm.name),
+          this.props.deviceSensors[i].name,
+          this.datastream_length
+        )
+      );
+    }
   }
 
-  function roundComponents(dataArray) {
-    return dataArray.map(function (element) {
-      return element % 1 === 0 ? element : element.toFixed(4);
+  getOptions(components, name, number) {
+    return {
+      chart: {
+        //type: 'spline',
+        animation: Highcharts.svg, // don't animate in old IE
+        marginRight: 10
+      },
+      boost: {
+        // chart-level boost when there are more than 2 series in the chart
+        useGPUTranslations: true,
+        seriesThreshold: 2
+      },
+      series: this.generateStartData(components, number),
+      title: {
+        text: name
+      },
+      xAxis: {
+        labels: {
+          enabled: false
+        }
+      },
+      yAxis: {
+        title: false,
+        labels: {
+          enabled: true
+        }
+      }
+    };
+  }
+
+  generateStartSeries(length) {
+    // generate an array of 'empty' data
+    var data = [],
+      time = new Date().getTime(),
+      i;
+
+    for (i = -length; i < 0; i += 1) {
+      data.push({
+        x: time + i * this.interval_length,
+        y: 0
+      });
+    }
+    return data;
+  }
+
+  generateStartData(components, length) {
+    //generate start data for the chart series
+    var all_series = [],
+      j;
+
+    for (let j = -components.length; j < 0; j += 1) {
+      all_series.push({
+        name: components[j + components.length],
+        data: this.generateStartSeries(length),
+        marker: {
+          enabled: false
+        }
+      });
+    }
+
+    return all_series;
+  }
+
+  componentDidMount() {
+    let diff = Highcharts.charts.length - this.props.selectedSensors.size; //for stopping and restarting
+    for (let i = 0; i < diff; i++) {
+      var shift = Highcharts.charts.shift(); //HighCharts does not delete 'deleted' charts but leaves an 'undefined' in the chart list
+    }
+    this.handleStartLiveUpdate();
+  }
+
+  componentWillUnmount() {
+    this.handleStopLiveUpdate();
+  }
+
+  updateLiveData() {
+    const setIter = this.props.selectedSensors[Symbol.iterator]();
+    var current;
+    for (var i = 0; i < Highcharts.charts.length; i++) {
+      current = setIter.next().value;
+      var chart = Highcharts.charts[i];
+
+      for (var j = 0; j < chart.series.length; j++) {
+        var series = chart.series[j];
+        var x;
+        var y;
+        if (Array.isArray(this.props.lastData[current])) {
+          x = this.props.lastData[current][0];
+          y = this.props.lastData[current][1][j];
+        } else {
+          x = new Date().getTime();
+          y = 0;
+        }
+        series.addPoint([x, y], true, true);
+      }
+    }
+  }
+
+  handleStartLiveUpdate(e) {
+    e && e.preventDefault();
+    this.setState({
+      liveUpdate: window.setInterval(this.updateLiveData, this.interval_length)
     });
   }
 
-  return (
-    <div>
-      <div className="panelHeader">4. Recording</div>
-      <div className="panelDivider"></div>
-      <Table style={{ tableLayout: 'fixed' }}>
-        <thead>
-          <tr className="bg-light">
-            <th>SensorName</th>
-            <th>Components</th>
-            <th>CurrentData</th>
-          </tr>
-        </thead>
-        <tbody>
-          {indices.map((sensorKey) => {
-            const sensorData = props.deviceSensors[sensorKey];
+  handleStopLiveUpdate(e) {
+    e && e.preventDefault();
+    window.clearInterval(this.state.liveUpdate);
+    this.setState({
+      liveUpdate: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="panelHeader">4. Recording</div>
+        <div className="panelDivider"></div>
+        <ul>
+          {Array.from(this.props.selectedSensors).map(sensorKey => {
             return (
-              <tr key={sensorKey}>
-                <td>{sensorData.name}</td>
-                <td>
-                  {sensorData.parseScheme.map((elm) => elm.name).join('; ')}
-                </td>
-                <td>
-                  {separateComponents(props.lastData[parseInt(sensorKey)])}
-                </td>
-              </tr>
+              <li key={sensorKey}>
+                <div>
+                  <HighchartsReact
+                    highcharts={Highcharts}
+                    options={this.allOptions[sensorKey]}
+                    updateArgs={[true, true, true]}
+                  />
+                </div>
+              </li>
             );
           })}
-        </tbody>
-      </Table>
-    </div>
-  );
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default BlePanelRecordingDisplay;
