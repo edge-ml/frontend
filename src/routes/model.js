af5392ec0799d4ce2b437c89186b6a896b4b8210
pir-anja <anja@ralfmartinhansen.de>
2022-03-28 15:52:22 +0200

    changed 'request access' button to beta link

--- src/routes/model.js
+++ src/routes/model.js
@@ -6,8 +6,8 @@ class ModelPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ready: true,
-      inviteRequested: false
+      ready: true
+      //inviteRequested: false
     };
   }
 
@@ -19,24 +19,28 @@ class ModelPage extends Component {
       <div className="w-100 h-100 d-flex justify-content-center align-items-center">
         <div className="card mt-5" style={{ border: '0px solid white' }}>
           <div className="card-body d-flex justify-content-center align-items-center flex-column">
-            <h4>Closed Beta</h4>
-            <div>edge-ml model generation is currently invite only.</div>
+            <h4>Under development</h4>
+            <div>
+              edge-ml model generation and other functionalities are currently
+              only available on beta.
+            </div>
             <button
               className="btn btn-secondary mt-3"
-              style={
+              /*style={
                 !this.state.inviteRequested
                   ? { display: 'block' }
                   : { display: 'none' }
-              }
+              }*/
               onClick={() => {
-                this.setState({
+                window.open('https://edge-ml-beta.dmz.teco.edu/', '_blank');
+                /*this.setState({
                   inviteRequested: true
-                });
+                });*/
               }}
             >
-              Request Access
+              Try it on beta now
             </button>
-            <div
+            {/*<div
               className="mt-4 w-20 text-success"
               style={
                 this.state.inviteRequested
@@ -47,7 +51,7 @@ class ModelPage extends Component {
               <small>
                 <i>Request sent! We'll let you know when you get access.</i>
               </small>
-            </div>
+            </div>*/}
           </div>
         </div>
       </div>
