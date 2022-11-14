daf9a12901a404b9d748c2537936ee43328442f5
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-11-14 10:15:17 +0100

    WIP: monile navbar

---
+++ src/AppView.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+import { Container } from 'reactstrap';
+
+import './AppView.css';
+
+const AppView = ({ navbar, content, mobileHeader }) => {
+  return (
+    <div className="position-relative">
+      <div className="position-fixed mobile-only" style={{ zIndex: 90 }}>
+        {mobileHeader}
+      </div>
+      <div className="d-flex">
+        <div className="mobile-exclude" style={{ minWidth: 160 }} />
+        <div
+          className="position-fixed mobile-navbar-slider"
+          style={{ zIndex: 100 }}
+        >
+          {navbar}
+        </div>
+        <Container>
+          <div className="mobile-only" style={{ minHeight: 50 }} />
+          {content}
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default AppView;
