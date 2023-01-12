0afb5750ba311be1f9abb1eeec1825ba6eb39c85
user <mark.schenkel123@gmail.com>
2023-01-12 16:06:45 +0100

    refactored ble connect panel

--- src/components/BLE/BlePanelConnectDevice.js
+++ src/components/BLE/BlePanelConnectDevice.js
@@ -16,21 +16,42 @@ function BlePanelConnectDevice(props) {
             justifyContent: 'space-between',
           }}
         >
-          <div>
-            {props.connectedBLEDevice ? (
-              <div>
-                The edge-ml firmware version is outdated. You can update it to
-                the latest version by clicking on the button.
-                <br />
-                Installed version:{' '}
-                <strong>{props.connectedDeviceData.installedFWVersion}</strong>
-                <br />
-                Latest version: <strong>{props.latestEdgeMLVersion}</strong>
-              </div>
-            ) : (
-              'No device connected'
-            )}
-          </div>
+          {props.connectedBLEDevice ? (
+            (<div>
+              <b>{props.connectedBLEDevice.name}</b>
+              {props.connectedBLEDevice.id}
+            </div>)(
+              props.isEdgeMLInstalled ? (
+                <div>
+                  {props.outdatedVersionInstalled && props.hasDFUFunction ? (
+                    <div>
+                      The edge-ml firmware version is outdated. You can update
+                      it to the latest version by clicking on the button.
+                      <br />
+                    </div>
+                  ) : null}
+                  Installed version:{' '}
+                  <strong>
+                    {props.connectedDeviceData.installedFWVersion}
+                  </strong>
+                  <br />
+                  Latest version: <strong>{props.latestEdgeMLVersion}</strong>
+                </div>
+              ) : props.hasDFUFunction ? (
+                <div>
+                  This device does not have the edge-ml firmware installed yet.
+                  You can install it by clicking on the button.
+                </div>
+              ) : (
+                <div className="text-danger">
+                  This device does not have the edge-ml firmware installed.
+                  Please install via the guide
+                </div>
+              )
+            )
+          ) : (
+            <div>Device not connected</div>
+          )}
           <SpinnerButton
             loadingtext={
               props.connectedBLEDevice ? 'Disconnecting...' : 'Connecting...'
@@ -41,65 +62,18 @@ function BlePanelConnectDevice(props) {
           >
             {props.connectedBLEDevice ? 'Disconnect device' : 'Connect device'}
           </SpinnerButton>
+          {(props.outdatedVersionInstalled || !props.isEdgeMLInstalled) &&
+          props.hasDFUFunction ? (
+            <Button color="primary" onClick={props.toggleDFUModal}>
+              Flash edge-ml firmware
+            </Button>
+          ) : null}
         </div>
         <small className="text-danger">
           <strong>Warning: </strong>
           If your device can not be found, try to turn bluetooth off and on
           again in your settings.
         </small>
-        {props.hasDFUFunction &&
-        props.connectedBLEDevice &&
-        ((props.isEdgeMLInstalled && props.outdatedVersionInstalled) ||
-          !props.isEdgeMLInstalled) ? (
-          <div>
-            <hr />
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              {props.isEdgeMLInstalled ? (
-                <div>
-                  The edge-ml firmware version is outdated. You can update it to
-                  the latest version by clicking on the button.
-                  <br />
-                  Installed version:{' '}
-                  <strong>{props.connectedDeviceData.generation}</strong>
-                  <br />
-                  Latest version: <strong>{props.latestEdgeMLVersion}</strong>
-                </div>
-              ) : (
-                <div>
-                  This device does not have the edge-ml firmware installed yet.
-                  You can install it by clicking on the button.
-                </div>
-              )}
-
-              <Button color="primary" onClick={props.toggleDFUModal}>
-                Flash edge-ml firmware
-              </Button>
-            </div>
-          </div>
-        ) : null}
-        {props.deviceNotUsable && props.connectedBLEDevice ? (
-          <div>
-            <hr />
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              <div className="text-danger">
-                This device does not have the edge-ml firmware installed. Please
-                install via the guide
-              </div>
-            </div>
-          </div>
-        ) : null}
       </CardBody>
     </Card>
   );
