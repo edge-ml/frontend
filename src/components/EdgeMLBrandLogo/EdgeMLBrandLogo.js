7dc4c84a84ac6789206e06cb7aab40fc1c69827b
Tobias King <tk.king.tk@gmail.com>
2023-02-13 21:46:28 +0100

    Fixed warning

--- src/components/EdgeMLBrandLogo/EdgeMLBrandLogo.js
+++ src/components/EdgeMLBrandLogo/EdgeMLBrandLogo.js
@@ -3,13 +3,17 @@ import { NavbarBrand } from 'reactstrap';
 import logoSvg from '../../logo.svg';
 
 const EdgeMLBrandLogo = ({ href }) => (
-  <NavbarBrand style={{ marginRight: '8px' }} className="dark-hover mt-2">
-    <a className="home-link" href={href}>
+  <NavbarBrand
+    style={{ marginRight: '8px' }}
+    className="dark-hover mt-2"
+    href={href}
+  >
+    <div className="home-link">
       <img style={{ marginRight: '8px', width: '32px' }} src={logoSvg} />
       <b>
         <div style={{ color: 'black' }}>edge-ml</div>
       </b>
-    </a>
+    </div>
   </NavbarBrand>
 );
 
