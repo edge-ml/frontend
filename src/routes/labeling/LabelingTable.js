25f7ece7eb8c231bd9520c90a253e5c58c722dfb
Mark Schenkel <mark.schenkel123@gmail.com>
2023-02-26 16:53:01 +0100

    labeling sets page does not display table, when no labeling set is available yet

--- src/routes/labeling/LabelingTable.js
+++ src/routes/labeling/LabelingTable.js
@@ -24,72 +24,76 @@ const LabelingTable = (props) => {
             </Button>
           </div>
         </div>
-        <div style={{ borderRadius: 10 }}>
-          <div className="datasets-header-wrapper d-flex d-flex justify-content-between flex-md-row flex-column align-content-baseline">
-            <div className="d-flex flex-row align-items-center p-1">
-              <div className="ml-md-2 mr-md-3 ">
-                <Checkbox
-                  isSelected={areAllSelected}
-                  onClick={(e) => {
-                    setAllSelected(!areAllSelected);
-                    if (areAllSelected) {
-                      props.deselectAll();
-                    } else {
-                      props.selectAll();
-                    }
-                  }}
-                ></Checkbox>
+        {props.labelings.length > 0 ? (
+          <div style={{ borderRadius: 10 }}>
+            <div className="datasets-header-wrapper d-flex d-flex justify-content-between flex-md-row flex-column align-content-baseline">
+              <div className="d-flex flex-row align-items-center p-1">
+                <div className="ml-md-2 mr-md-3 ">
+                  <Checkbox
+                    isSelected={areAllSelected}
+                    onClick={(e) => {
+                      setAllSelected(!areAllSelected);
+                      if (areAllSelected) {
+                        props.deselectAll();
+                      } else {
+                        props.selectAll();
+                      }
+                    }}
+                  ></Checkbox>
+                </div>
+                <Button
+                  className="ml-3 btn-delete"
+                  id="deleteDatasetsButton"
+                  size="sm"
+                  color="secondary"
+                  onClick={props.onClickDeleteButton}
+                >
+                  <FontAwesomeIcon
+                    className="mr-2"
+                    icon={faTrashAlt}
+                  ></FontAwesomeIcon>
+                  Delete
+                </Button>
+                <Button
+                  id="selectAllEmptyButton"
+                  size="sm"
+                  color="secondary"
+                  onClick={props.selectAllEmpty}
+                  className="ml-2"
+                >
+                  <FontAwesomeIcon
+                    className="mr-2"
+                    icon={faFilter}
+                  ></FontAwesomeIcon>
+                  Select Empty Labeling Sets
+                </Button>
               </div>
-              <Button
-                className="ml-3 btn-delete"
-                id="deleteDatasetsButton"
-                size="sm"
-                color="secondary"
-                onClick={props.onClickDeleteButton}
-              >
-                <FontAwesomeIcon
-                  className="mr-2"
-                  icon={faTrashAlt}
-                ></FontAwesomeIcon>
-                Delete
-              </Button>
-              <Button
-                id="selectAllEmptyButton"
-                size="sm"
-                color="secondary"
-                onClick={props.selectAllEmpty}
-                className="ml-2"
-              >
-                <FontAwesomeIcon
-                  className="mr-2"
-                  icon={faFilter}
-                ></FontAwesomeIcon>
-                Select Empty Labeling Sets
-              </Button>
+            </div>
+            <div
+              className="w-100 position-relative"
+              style={{
+                border: '2px solid rgb(230, 230, 234)',
+                borderRadius: '0px 0px 10px 10px',
+                overflow: 'hidden',
+              }}
+            >
+              {props.labelings.map((labeling, index) => (
+                <LabelingTableEntry
+                  key={labeling._id}
+                  labeling={labeling}
+                  labels={props.labels}
+                  onClickEdit={props.onClickEdit}
+                  index={index}
+                  isSelected={props.labelingsToDelete.includes(labeling['_id'])}
+                  toggleCheck={props.toggleCheck}
+                  onClickDeleteLabelingIcon={props.onClickDeleteLabelingIcon}
+                />
+              ))}
             </div>
           </div>
-          <div
-            className="w-100 position-relative"
-            style={{
-              border: '2px solid rgb(230, 230, 234)',
-              borderRadius: '0px 0px 10px 10px',
-              overflow: 'hidden',
-            }}
-          >
-            {props.labelings.map((labeling, index) => (
-              <LabelingTableEntry
-                key={labeling._id}
-                labeling={labeling}
-                labels={props.labels}
-                onClickEdit={props.onClickEdit}
-                index={index}
-                isSelected={props.labelingsToDelete.includes(labeling['_id'])}
-                toggleCheck={props.toggleCheck}
-                onClickDeleteLabelingIcon={props.onClickDeleteLabelingIcon}
-              />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <div>{'No labeling sets available yet.'}</div>
+        )}
       </Fragment>
     </div>
   );
