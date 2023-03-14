e18e5ae2e1ec73138df40fdf5cf807fae5b4067a
Tobias King <tk.king.tk@gmail.com>
2023-03-14 12:02:32 +0100

    Removed trailing slash from route which redirected to http

--- src/services/ApiServices/DatasetLabelService.js
+++ src/services/ApiServices/DatasetLabelService.js
@@ -13,7 +13,7 @@ export const createDatasetLabel = (datasetId, labelingId, label) => {
         apiConsts.HTTP_METHODS.POST,
         apiConsts.DATASET_STORE,
         apiConsts.DATASET_STORE_ENDPOINTS.DATASET_LABELINGS +
-          `${datasetId}/${labelingId}/`,
+          `${datasetId}/${labelingId}`,
         label
       )
     )
