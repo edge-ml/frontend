0904ef2293d9eae003d4f0e8ebc3da2ce5361bcb
KtrauM <murat.kurnaz12@gmail.com>
2023-06-20 00:26:52 +0200

    Fix labeling selection card

--- src/components/BLE/BleLabelingMenu.js
+++ src/components/BLE/BleLabelingMenu.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardHeader, CardBody, Dropdown, DropdownMenu, DropdownItem, DropdownToggle, Badge } from 'reactstrap';
 
-export const BleLabelingMenu = ({ labelings, selectedLabeling, selectedLabel, handleSelectLabeling, handleSelectLabel, shortcutKeys }) => {
+export const BleLabelingMenu = ({ labelings, selectedLabeling, handleSelectLabeling, handleSelectLabel, shortcutKeys }) => {
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const toggleDropdown = () => {
@@ -9,38 +9,38 @@ export const BleLabelingMenu = ({ labelings, selectedLabeling, selectedLabel, ha
     }
 
     return (
-        <Card className="text-left">
-          <CardHeader>
-            <h4>4. Labelings</h4>
-          </CardHeader>
-          <CardBody className='d-flex flex-column'>
-            <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
-                <DropdownToggle caret>Labelings</DropdownToggle>
-                <DropdownMenu>
-                    {/* <DropdownItem header>Labelings</DropdownItem> */}
-                    {labelings.map(labeling => (
-                        <DropdownItem key={labeling.name} onClick={e => handleSelectLabeling(labeling)}>{labeling.name}</DropdownItem>
-                    ))}
-                </DropdownMenu>
-            </Dropdown>
-            <div className='d-flex mb-2'>
-            {selectedLabeling && selectedLabeling.labels.map((label, labelIdx) => (
-                <div key={label._id} className='d-flex flex-column align-items-center mr-1'>
-                    <Badge 
-                        style={{
-                            backgroundColor: label.color, 
-                            border: label._id === selectedLabel?._id ? '0.2em outset': '0.2em solid transparent'}}
-                        onClick={e => handleSelectLabel(label)}
-                    >
-                        {label.name}
-                    </Badge>
-                    <span>{shortcutKeys[labelIdx]}</span>
-                </div>
-            ))}
+        <div className="mr-2 mt-2" >
+            <div className="header-wrapper d-flex justify-content-between align-content-center ">
+                <h4>4. Labelings</h4>
+                <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} className='position-relative p-0'>
+                    <DropdownToggle caret>Labelings</DropdownToggle>
+                    <DropdownMenu>
+                        {/* <DropdownItem header>Labelings</DropdownItem> */}
+                        {labelings.map(labeling => (
+                            <DropdownItem key={labeling.name} onClick={e => handleSelectLabeling(labeling)}>{labeling.name}</DropdownItem>
+                        ))}
+                    </DropdownMenu>
+                </Dropdown>
             </div>
-            <span>To start/stop labeling the data during recording:</span>
-            <span>Press the shortcut key on the keyboard which corresponds the desired label.</span>
-          </CardBody>
-        </Card>
+            <div className="body-wrapper p-3 d-flex flex-column">
+                <div className='d-flex mb-2'>
+                    {selectedLabeling && selectedLabeling.labels.map((label, labelIdx) => (
+                        <div key={label._id} className='d-flex flex-column align-items-center mr-1'>
+                            <Badge
+                                style={{
+                                    backgroundColor: label.color,
+                                }}
+                                onClick={e => handleSelectLabel(label)}
+                            >
+                                {label.name}
+                            </Badge>
+                            <span>{shortcutKeys[labelIdx]}</span>
+                        </div>
+                    ))}
+                </div>
+                <span>To start/stop labeling the data during recording:</span>
+                <span>Press the shortcut key on the keyboard which corresponds the desired label.</span>
+            </div>
+        </div>
     )
 }
