61bc566752397734f3af8b2b4186152efd27cc8e
KtrauM <murat.kurnaz12@gmail.com>
2023-06-22 10:18:33 +0200

    Add wrapping if the labels don't fit a single line

--- src/components/BLE/BleLabelingMenu.js
+++ src/components/BLE/BleLabelingMenu.js
@@ -23,7 +23,7 @@ export const BleLabelingMenu = ({ labelings, selectedLabeling, handleSelectLabel
                 </Dropdown>
             </div>
             <div className="body-wrapper p-3 d-flex flex-column">
-                <div className='d-flex mb-2'>
+                <div className='d-flex mb-2 flex-wrap'>
                     {selectedLabeling && selectedLabeling.labels.map((label, labelIdx) => (
                         <div key={label._id} className='d-flex flex-column align-items-center mr-1'>
                             <Badge
