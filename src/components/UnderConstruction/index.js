d3ae21a14a5f344a45d13cbc9e893b203a68c2e7
Tobias King <tk.king.tk@gmail.com>
2023-11-07 16:32:47 +0100

    Updated text for production ml-part

--- src/components/UnderConstruction/index.js
+++ src/components/UnderConstruction/index.js
@@ -1,17 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faToolbox } from '@fortawesome/free-solid-svg-icons';
+import EdgeMLBrandLogo from '../EdgeMLBrandLogo/EdgeMLBrandLogo';
+import logoSvg from '../../logo.svg';
 
 import './index.css';
 
 const UnderConstruction = () => {
   return (
     <div class="wrapper">
-      <FontAwesomeIcon
-        icon={faToolbox}
-        className="icon"
-        size="10x"
-      ></FontAwesomeIcon>
-      <div className="text">Under Construction</div>
+      <div className="d-flex justify-content-center align-items-center m-5">
+        <img style={{ width: '150px' }} src={logoSvg} />
+        <div className="text">edge-ml</div>
+      </div>
+      <div className="text text-center">
+        Check out machine learning on{' '}
+        <a href="https://beta.edge-ml.org" target="_blank">
+          edge-ml beta
+        </a>
+      </div>
     </div>
   );
 };
