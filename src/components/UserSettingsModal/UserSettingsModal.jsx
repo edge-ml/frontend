271f25023e3fef6971c815e9922e9adc144dc20f
Tobias King <tk.king.tk@gmail.com>
2024-07-23 12:05:28 +0200

    Fixed error

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -5,24 +5,35 @@ import PasswordSettings from "./PasswordSettings";
 import UserNameSettings from "./UserNameSettings";
 import DeleteUser from "./DeleteUser";
 import UserSettingsProvider from "./UserSettingsProvider";
+import useUserStore from "../../Hooks/useUser";
 
-const UserSettingsModal = ({isOpen, onClose}) => {
+const UserSettingsModal = ({ isOpen, onClose }) => {
+  const user = useUserStore((state) => state.user);
+  console.log(user);
   return (
-    <Modal size="lg" isOpen={isOpen} onClose={onClose} className="modal-dialog-scrollable">
+    <Modal
+      size="lg"
+      isOpen={isOpen}
+      onClose={onClose}
+      className="modal-dialog-scrollable"
+    >
       <ModalHeader style={{ borderBottom: "None" }}>User Settings</ModalHeader>
       <ModalBody style={{ maxHeight: "calc(100vh)", overflowY: "auto" }}>
-        <MailSettings id="mailSettings" />
-        <hr />
-        <PasswordSettings id="passwordSettings" />
-        <hr />
-        <UserNameSettings id="userNameSettings" />
-        <hr />
+        {!user.provider || user.provider === "local" ? (
+          <>
+            <MailSettings id="mailSettings" />
+            <hr />
+            <PasswordSettings id="passwordSettings" />
+            <hr />
+            <UserNameSettings id="userNameSettings" />
+            <hr />
+          </>
+        ) : null}
         <UserSettingsProvider>
           <DeleteUser />
         </UserSettingsProvider>
       </ModalBody>
-      <ModalFooter>
-      </ModalFooter>
+      <ModalFooter></ModalFooter>
     </Modal>
   );
 };
